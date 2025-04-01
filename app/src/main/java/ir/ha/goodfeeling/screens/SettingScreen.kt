d09ed7c82d772bbfb68e221d1d2d1d1ab744ff6e
HasanAzimi <ihasan.azimi@gmail.com>
2025-04-01 05:34:52 +0330

    modify some screens

--- app/src/main/java/ir/ha/goodfeeling/screens/SettingScreen.kt
+++ app/src/main/java/ir/ha/goodfeeling/screens/SettingScreen.kt
@@ -1,18 +1,28 @@
 package ir.ha.goodfeeling.screens
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import ir.ha.goodfeeling.ui.theme.GoodFeelingTheme
 
 
 @Composable
 fun SettingScreen(modifier: Modifier = Modifier) {
     Surface {
-        Box {
-
+        Box(
+            modifier = modifier
+                .fillMaxSize()
+                .padding(16.dp),
+            contentAlignment = Alignment.Center
+        ) {
+            Text("محتوای اصلی صفحه")
         }
     }
 }
